fff492f0d26eae08c4031c7dbc594e829c01058d
Chech_ap28@student.itstep.org <Chech_ap28@student.itstep.org>
2024-10-16 13:47:44 +0300

    Add project files.

---
+++ 16.10.2024.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include "Class_Templ.h"
+using namespace std;
+
+class Point
+{
+	int x = 0;
+	int y = 0;
+public:
+	Point() = default;
+	Point(int a, int b): x(a), y(b){}
+	Point operator+(const Point& b)
+	{
+		return Point(x + b.x, y + b.y);
+	}
+	int GetX() const { return x; }
+	int GetY() const { return y; }
+};
+
+ostream& operator<< (ostream& os, const Point& obj)
+{
+	os << obj.GetX() << ":" << obj.GetY();
+
+	return os;
+}
+
+template<class T>
+void Sum(T a, T b)
+{
+	cout << a << " + " << b << " = " << a + b << endl;
+}
+
+int main()
+{
+	srand(time(0));
+	Sum(2, 3);
+	Sum(2.5, 6.7);
+}
